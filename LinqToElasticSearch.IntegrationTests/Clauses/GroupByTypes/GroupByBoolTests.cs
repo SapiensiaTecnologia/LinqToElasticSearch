bc610cca6a392759a3fe37403834a9a7e358a476
Marcio Costa <marcio.costa@sapiensia.com>
2022-08-18 12:17:29 -0300

    Creating integration tests to bool aggreation

---
+++ LinqToElasticSearch.IntegrationTests/Clauses/GroupByTypes/GroupByBoolTests.cs
@@ -0,0 +1,84 @@
+﻿using System.Linq;
+using AutoFixture;
+using FluentAssertions;
+using Xunit;
+
+namespace LinqToElasticSearch.IntegrationTests.Clauses
+{
+    
+    public class GroupByBoolTests : IntegrationTestsBase<SampleData>
+    {
+        
+        [Fact]
+        public void GroupByBoolWithUniqueGrouping()
+        {
+            //Given
+            var datas = Fixture.CreateMany<SampleData>(3).ToList();
+            
+            datas[0].Can = true;
+            datas[1].Can = true;
+            datas[2].Can = false;
+            
+            Bulk(datas);
+            ElasticClient.Indices.Refresh();
+            
+            //When
+            var results = Sut.GroupBy(x => x.Can).ToList();
+            
+            //Then
+            results.Count.Should().Be(2);
+            results.Should().ContainSingle(x =>
+                x.Key == true
+                && x.Count() == 2);
+            
+            results.Should().ContainSingle(x =>
+                x.Key == false
+                && x.Count() == 1);
+        }
+
+        [Fact]
+        public void GroupByBoolWithWithMultipleGrouping()
+        {
+            //Given
+            var datas = Fixture.CreateMany<SampleData>(4).ToList();
+            
+            datas[0].Can = true;
+            datas[0].Age = 3;
+            
+            datas[1].Can = true;
+            datas[1].Age = 3;
+            
+            datas[2].Can = true;
+            datas[2].Age = 4;
+            
+            datas[3].Can = false;
+            datas[3].Age = 5;
+
+            Bulk(datas);
+            ElasticClient.Indices.Refresh();
+
+            //When
+            var results = Sut.GroupBy(x => new {x.Can, x.Age}).ToList();
+            
+            //Then
+            results.Count.Should().Be(3);
+            results.Should().ContainSingle(x =>
+                x.Key.Can == true
+                && x.Key.Age == 3
+                && x.Count() == 2);
+            
+            results.Should().ContainSingle(x =>
+                x.Key.Can == true
+                && x.Key.Age == 4
+                && x.Count() == 1);
+            
+            results.Should().ContainSingle(x =>
+                x.Key.Can == false
+                && x.Key.Age == 5
+                && x.Count() == 1);
+            
+        }
+        
+        
+    }
+}
